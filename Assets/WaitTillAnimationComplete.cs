ffe9fd67befaa5c797d05ac23eea21d3ff817b08
Maansi Manchanda <maansima@usc.edu>
2025-12-03 23:44:05 -0500

    updated drive up animation

---
+++ Assets/WaitTillAnimationComplete.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitTillAnimationComplete : MonoBehaviour
+{
+    public BoxCollider colliderToEnable;
+
+    void Start()
+    {
+        // Make sure collider starts OFF
+        if (colliderToEnable != null)
+            colliderToEnable.enabled = false;
+    }
+
+    // This function will be called by an Animation Event
+    public void TurnOnCollider()
+    {
+        if (colliderToEnable != null)
+            colliderToEnable.enabled = true;
+    }
+}
